16ce02d6d4eed802b8989ae1706fc6efa3f6f953
Farhod Gofurov <fg325609@gmail.com>
2026-06-03 15:10:59 +0500

    lll

---
+++ src/companents/Sectionone/Sectionone.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Sectionone.scss"
+const Sectionone = () => {
+      const { t } = useTranslation();
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Sectionone
