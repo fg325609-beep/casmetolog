10b199ebbfe4975b88d0e9d7182caf797f6151ac
Farhod Gofurov <fg325609@gmail.com>
2026-06-03 08:05:47 +0500

    llll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './companents/Header/Header';
+import Hero from './companents/Hero/Hero';
+
 
 
 const App = () => {
   return (
     <div>
   <Header/>
-      
+      <Hero/>
     </div>
   );
 };
--- src/companents/Hero/Hero.jsx
+++ src/companents/Hero/Hero.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import "./hero.scss"
-const Hero = () => {
-  return (
-    <div>
-        <div className="container">
-            <section className='hero'>
-                <div className="hero-left"></div>
-                <div className="hero-right">
-                    <img src="src/companents/img/hero (2).png" alt="" />
-                </div>
-                </section>
-        </div>
-    </div>
-  )
-}
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import "./hero.scss";
 
-export default Hero
+const Hero = () => {
+  const { t } = useTranslation();
+
+  return (
+    <section className="hero-section">
+      <div className="container hero-container">
+        
+        {/* Chap tomon: Matnlar va Tugmalar */}
+        <div className="hero-left">
+          <span className="hero-subtitle">{t('hero_sub')}</span>
+          <h1 className="hero-title">{t('hero_title')}</h1>
+          <p className="hero-description">{t('hero_desc')}</p>
+          
+          <div className="hero-buttons">
+            <button className="btn btn-primary">{t('btn_reg')}</button>
+            <button className="btn btn-secondary">{t('btn_more')}</button>
+          </div>
+        </div>
+
+        {/* O'ng tomon: Kosmetologlar rasmi */}
+        <div className="hero-right">
+          <div className="hero-image-wrapper">
+            <img src="src/companents/img/hero (2).png" alt="Kosmetologlar" className="hero-img" />
+          </div>
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
